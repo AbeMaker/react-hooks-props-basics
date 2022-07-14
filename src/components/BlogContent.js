96dede4059e09ee6d3ec44f32d3b5759ff1973db
Abraham Anyang <anyangmaker90@gmail.com>
2022-07-14 18:26:14 +0300

    basics learned well

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function BlogContent(props) {
+  console.log(props);//This helps us to view the datatype of the func as well as showing the contents
   return <div id="blog-content">{props.articleText}</div>;
 }
 
